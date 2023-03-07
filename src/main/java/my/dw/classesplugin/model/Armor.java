b8c2a9594ba5e1ee9528becff23c7d67283e4455
Quintic57 <dwinata578@gmail.com>
2023-03-06 23:15:54 -0500

    feature: weapons and armor (WIP)
    split weapons and armor into seperate enums to increase readability, implement weapons/armor for all classes.

---
+++ src/main/java/my/dw/classesplugin/model/Armor.java
@@ -0,0 +1,21 @@
+package my.dw.classesplugin.model;
+
+import my.dw.classesplugin.model.abilities.AssassinCloakAbility;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
+public enum Armor {
+    ASSASSIN(),
+    SWORDSMAN(),
+    ARCHER(),
+    JUGGERNAUT(),
+    SPECIALIST(),
+    SCOUT(),
+    CAPTAIN(),
+    ALCHEMIST(),
+    KNIGHT();
+}
--- src/main/java/my/dw/classesplugin/model/Class.java
+++ src/main/java/my/dw/classesplugin/model/Class.java
@@ -4,6 +4,8 @@ import my.dw.classesplugin.model.abilities.Ability;
 import my.dw.classesplugin.model.abilities.AbilityType;
 import my.dw.classesplugin.model.abilities.AssassinCloakAbility;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,26 +13,76 @@ import java.util.List;
 import java.util.Map;
 
 public enum Class {
-    ASSASSIN(List.of(new AssassinCloakAbility())),
-    SWORDSMAN(List.of()),
-    ARCHER(List.of()),
-    JUGGERNAUT(List.of()),
-    SPECIALIST(List.of()),
-    SCOUT(List.of()),
-    CAPTAIN(List.of()),
-    ALCHEMIST(List.of()),
-    KNIGHT(List.of());
+    ASSASSIN(
+            Weapon.ASSASSIN.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of(new AssassinCloakAbility())
+    ),
+    SWORDSMAN(
+            Weapon.SWORDSMAN.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    ARCHER(Weapon.ARCHER.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    JUGGERNAUT(Weapon.JUGGERNAUT.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    SPECIALIST(Weapon.SPECIALIST.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    SCOUT(Weapon.SCOUT.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    CAPTAIN(Weapon.CAPTAIN.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    ALCHEMIST(Weapon.ALCHEMIST.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    ),
+    KNIGHT(Weapon.KNIGHT.getWeapon(),
+            List.of(),
+            List.of(),
+            List.of()
+    );
 
     // TODO: Add inventory for each class, and implement all respective abilities
-    private final List<Material> inventory;
+    private final ItemStack weapon;
+    private final List<ItemStack> armor;
+    private final List<ItemStack> inventory;
     private final List<Ability> abilities;
 
-    Class(List<Ability> abilities) {
+    Class(ItemStack weapon, List<ItemStack> armor, List<ItemStack> inventory, List<Ability> abilities) {
+        this.weapon = weapon;
+        this.armor = armor;
         this.inventory = new ArrayList<>();
         this.abilities = abilities;
     }
 
-    public List<Material> getInventory() {
+    public ItemStack getWeapon() {
+        return weapon;
+    }
+
+    public List<ItemStack> getArmor() {
+        return armor;
+    }
+
+    public List<ItemStack> getInventory() {
         return inventory;
     }
 
